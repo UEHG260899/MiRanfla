37c73664b765b6ca209b03ba5f5abd5e02c9bce0
Uriel Enrique <urielenrique99@gmail.com>
2024-08-27 18:07:58 -0600

    Initial Commit

---
+++ MiRanfla/MiRanflaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiRanflaApp.swift
+//  MiRanfla
+//
+//  Created by Uriel Hernandez Gonzalez on 27/08/24.
+//
+
+import SwiftUI
+
+@main
+struct MiRanflaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
