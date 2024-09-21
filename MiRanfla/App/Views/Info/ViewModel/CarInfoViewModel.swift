0ae92980ac3c0119501fecd56398cb9466c2df28
Uriel Hernandez <urielenrique99@gmail.com>
2024-09-21 00:34:41 +0000

    Adding functionallity on CarInfoView

---
+++ MiRanfla/App/Views/Info/ViewModel/CarInfoViewModel.swift
@@ -0,0 +1,64 @@
+//
+//  CarInfoViewModel.swift
+//  MiRanfla
+//
+//  Created by Uriel Hernandez Gonzalez on 20/09/24.
+//
+
+import Observation
+
+@Observable
+final class CarInfoViewModel {
+    
+    private let adapter: CarAdapting
+    private let notificationsManager: NotificationsManager
+    
+    let shouldShowNotificationsRow: Bool
+    var uiCar: UICar {
+        didSet {
+            Task {
+                await configureNotifications()
+            }
+        }
+    }
+    var showError = false
+    
+    
+    init(uiCar: UICar, adapter: any CarAdapting, notificationsManager: NotificationsManager) {
+        self.uiCar = uiCar
+        self.adapter = adapter
+        self.notificationsManager = notificationsManager
+        self.shouldShowNotificationsRow = Constants.requiredVerificationStates.contains(uiCar.plateState)
+    }
+
+    func configureNotifications() async {
+        do {
+            let permissionsGranted = try await notificationsManager.requestPermissions()
+            
+            if !permissionsGranted {
+                uiCar.verificationNotificationsEnabled = false
+                return
+            }
+            
+            if uiCar.verificationNotificationsEnabled {
+                let verificationMonths = VerificationMonths(forPlate: uiCar.lastPlateNumber)
+                let firstNotification = LocalNotification(id: uiCar.id.uuidString,
+                                                          month: verificationMonths.first.rawValue,
+                                                          title: "¡No te olvides de verificar!",
+                                                          body: "Tu \(uiCar.make) \(uiCar.model) verifica este mes.")
+                let secondNotification = LocalNotification(id: uiCar.id.uuidString,
+                                                          month: verificationMonths.second.rawValue,
+                                                          title: "¡No te olvides de verificar!",
+                                                          body: "Tu \(uiCar.make) \(uiCar.model) verifica este mes.")
+                try await notificationsManager.schedule(firstNotification)
+                try await notificationsManager.schedule(secondNotification)
+                return
+            }
+            
+            notificationsManager.removeNotifications(for: uiCar.id.uuidString)
+        
+        } catch {
+            showError = true
+        }
+    }
+}
---
+++ MiRanflaTests/CarInfoViewModelTests.swift
@@ -0,0 +1,84 @@
+//
+//  CarInfoViewModelTests.swift
+//  MiRanflaTests
+//
+//  Created by Uriel Hernandez Gonzalez on 20/09/24.
+//
+
+@testable import MiRanfla
+import XCTest
+
+final class CarInfoViewModelTests: XCTestCase {
+    
+    var car: UICar!
+    var sut: CarInfoViewModel!
+    var mockNotificationsCenter: MockUNNotificationCenter!
+    var mockAdapter: MockCarAdapter!
+    
+    override func setUp() {
+        super.setUp()
+        car = .empty
+        mockAdapter = MockCarAdapter()
+        mockNotificationsCenter = MockUNNotificationCenter()
+        let notificationsManager = NotificationsManager(notificationCenter: mockNotificationsCenter)
+        sut = CarInfoViewModel(uiCar: car, adapter: mockAdapter, notificationsManager: notificationsManager)
+    }
+    
+    override func tearDown() {
+        sut = nil
+        mockAdapter = nil
+        mockNotificationsCenter = nil
+        car = nil
+        super.tearDown()
+    }
+    
+    func testInitialValues() {
+        XCTAssertFalse(sut.showError)
+    }
+
+    func testShouldShowNotificationsRow() {
+        XCTAssertTrue(sut.shouldShowNotificationsRow)
+        
+        car = UICar(id: .init(), make: "", model: "", year: "", lastPlateNumber: "", milage: "", tankCapacity: "", plateState: .chiapas, verificationNotificationsEnabled: false)
+        sut = CarInfoViewModel(uiCar: car, adapter: mockAdapter, notificationsManager: NotificationsManager(notificationCenter: mockNotificationsCenter))
+        
+        XCTAssertFalse(sut.shouldShowNotificationsRow)
+    }
+
+    func testConfigureNotifications() async {
+        // Notifications are enabled AND permissions throw
+        sut.uiCar.verificationNotificationsEnabled = true
+        mockNotificationsCenter.authorizationReturn = .failure(NSError(domain: "com.miranfla.tests", code: 10))
+                
+        try? await Task.sleep(for: .milliseconds(50))
+        
+        XCTAssertTrue(sut.showError)
+        
+        // Notifications are enabled AND permissions are denied
+        sut.uiCar.verificationNotificationsEnabled = true
+        mockNotificationsCenter.authorizationReturn = .success(false)
+        
+        try? await Task.sleep(for: .milliseconds(50))
+        
+        XCTAssertFalse(sut.uiCar.verificationNotificationsEnabled)
+        
+        // Notifications are enabled AND permissions are granted
+        sut.uiCar.verificationNotificationsEnabled = true
+        mockNotificationsCenter.authorizationReturn = .success(true)
+        
+        try? await Task.sleep(for: .milliseconds(50))
+        
+        XCTAssertTrue(mockNotificationsCenter.calledMethods.contains(.add))
+        XCTAssertEqual(mockNotificationsCenter.receivedNotificationRequest?.identifier, car.id.uuidString)
+        XCTAssertEqual(mockNotificationsCenter.receivedNotificationRequest?.content.title, "¡No te olvides de verificar!")
+        XCTAssertEqual(mockNotificationsCenter.receivedNotificationRequest?.content.body, "Tu \(car.make) \(car.model) verifica este mes.")
+        
+        // Notifications are disabled AND permissions are granted
+        sut.uiCar.verificationNotificationsEnabled = false
+        
+        try? await Task.sleep(for: .milliseconds(50))
+        
+        XCTAssertTrue(mockNotificationsCenter.calledMethods.contains(.removeDeliveredNotifications))
+        XCTAssertTrue(mockNotificationsCenter.calledMethods.contains(.removePendingNotificationRequests))
+    }
+}
